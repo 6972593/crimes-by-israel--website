2812cb22c3e41f93b101ae43755b0d27261df5b1
6972593 <6972593.anon@gmail.com>
2024-01-07 15:36:59 -0600

    enhance mobile view with images of pages

---
+++ manage_content.js
@@ -0,0 +1,57 @@
+const PDF_HTML = '<iframe src="crimes-by-israel-30-dec-2023.pdf"></iframe>'
+const WIDTH_THRESHOLD = 600;
+const CONTENT_CONTAINER_ID = 'contentContainer';
+const PDF_CONTAINER_CLASS = 'pdf-container';
+const IMAGES_CONTAINER_CLASS = 'images-container';
+const PAGES_DIR = 'pages';
+const PAGE_COUNT_FILE = 'page-count.txt';
+const PAGE_FILE_BASE_NAME = 'page_'
+var prevWidth = window.innerWidth;
+
+function isMobileDevice() {
+    return window.innerWidth <= WIDTH_THRESHOLD;
+}
+
+function prevIsMobileDevice() {
+    return prevWidth <= WIDTH_THRESHOLD;
+}
+
+function widthThresholdCrossed() {
+    if (isMobileDevice() == prevIsMobileDevice()) {
+        return false;
+    } else {
+        prevWidth = window.innerWidth;
+        return true;
+    }
+}
+
+function loadImages(pageCount) {
+    const container = document.getElementById(CONTENT_CONTAINER_ID);
+    container.innerHTML = ''; // clear existing content
+    container.className = IMAGES_CONTAINER_CLASS;
+    for (let i=1; i<=pageCount; i++) {
+        const img = document.createElement('img');
+        img.src = `${PAGES_DIR}/${PAGE_FILE_BASE_NAME}${i}.png`;
+        container.appendChild(img);
+    }
+}
+
+function setup() {
+    if (isMobileDevice()) {
+        fetch(`${PAGES_DIR}/${PAGE_COUNT_FILE}`)
+            .then(response => response.text())
+            .then(text => {
+                const pageCount = parseInt(text, 10);
+                if (!isNaN(pageCount)) {
+                    loadImages(pageCount);
+                }
+            });
+    } else {
+        const container = document.getElementById(CONTENT_CONTAINER_ID);
+        container.innerHTML = PDF_HTML;
+        container.className = PDF_CONTAINER_CLASS;
+    }
+}
+
+window.onload = setup;
+window.onresize = () => { if (widthThresholdCrossed()) {setup();} };
